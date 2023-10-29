7969272951530583f294fd72aa05b64b9c6ca127
Farid Zainulin <10221278@mail.ru>
2023-10-29 16:18:44 +0300

    allowed update

--- hexlet-code/task_manager/settings.py
+++ hexlet-code/task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['task-manager-dj.onrender.com', 'webserver', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['https://task-manager-dj.onrender.com', 'webserver', '127.0.0.1', 'localhost']
 
 
 # Application definition
