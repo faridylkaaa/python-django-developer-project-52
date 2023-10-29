866a622f19eda29dbac755eabddda9ff15963944
Farid Zainulin <10221278@mail.ru>
2023-10-29 14:58:59 +0300

    Добавил переменные окружения + requirements.txt

--- hexlet-code/task_manager/settings.py
+++ hexlet-code/task_manager/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f0*4is!upk!#94xp$&8u%w=!-sp#uxfwivljd*bc$t6sgftml1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['webserver', '127.0.0.1']
 
